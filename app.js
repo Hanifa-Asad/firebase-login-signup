aba008de18a26f8f5863c20f69b1aa9fc608d67d
Hanifa-Asad <hanifaasasd23@gmail.com>
2024-11-08 02:17:47 +0500

    committed

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-import {  
-    auth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    GoogleAuthProvider,
-    provider,
-    signOut,
-  
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  provider,
+  signOut,
+  getFirestore,
+  db,
+  collection,
+  addDoc,
+  getDocs, doc, setDoc
 } from "./firebase.js";
 // var email = document.getElementById("email");
-export{signUp, logIn}
+export { signUp, logIn }
 // let signUp = () => {
 //     let email = document.getElementById("email").value;
 //     let password = document.getElementById("pswd").value;
@@ -21,19 +25,19 @@ export{signUp, logIn}
 //         console.log("test");
 //         createUserWithEmailAndPassword(auth, email, password)
 //   .then((userCredential) => {
-   
+
 //     const user = userCredential.user;
 //     console.log(user);
-  
+
 //     swal("Account created successfully");
-    
+
 //   })
 //   .catch((error) => {
 //     console.log(error.message);
 //     swal("error.code");
-   
+
 //   });
-        
+
 //     } else{
 //         swal("Invalid email or Password");
 //     }
@@ -43,134 +47,232 @@ export{signUp, logIn}
 //     }
 // };
 let signUp = () => {
-   let email = document.getElementById("email").value;
-    let password = document.getElementById("pswd").value;
-    let cPassword = document.getElementById("cpswd").value;
-    let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    let passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-  
-    if (emailRegex.test(email) && passwordRegex.test(password)) {
-      console.log("test");
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          const user = userCredential.user;
-          console.log(user);
-          localStorage.setItem("userEmail", email)
-          // alert("Account created successfully");
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Account created successfully!",
-            showConfirmButton: false,
-            timer: 1500,
-          });
-          window.location.href = "./dashboard/index.html"
-        })
-        .catch((error) => {
-          console.log(error.message);
-          alert(error.code);
-        });
-    } else {
-      // alert("Invalid email or Password");
-      Swal.fire({
-        title: "Invalid",
-        text: "Invalid Email or Password",
-        icon: "question",
-      });
-    }
-    if (password !== cPassword) {
-      // alert("Passwords should be identical");
-      Swal.fire({
-        title: "Not Match",
-        text: "IPasswords should be identical",
-        icon: "question",
-      });
-    }
-  };
-if(window.location.pathname=="/index.html"){
-    let signUp_btn = document.getElementById("signUp_btn");
-    signUp_btn.addEventListener("click", signUp);
-}
-// let signUp_btn = document.getElementById("signUp_btn");
-//     signUp_btn.addEventListener("click", signUp);
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("pswd").value;
+  let cPassword = document.getElementById("cpswd").value;
+  let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  let passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
-
-  
-  let logIn = () => {
-    let email = document.getElementById("Email").value;
-    let password = document.getElementById("pass").value;
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+  let name = document.getElementById("name").value;
+  let number = document.getElementById("number").value;
+  let userData = { name, number, email, password };
+  console.log(userData);
+  if (emailRegex.test(email) && passwordRegex.test(password)) {
+    console.log("test");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async(userCredential) => {
         const user = userCredential.user;
         console.log(user);
-        
-        // alert("Login Successful")
+        localStorage.setItem("userEmail", email)
+        // alert("Account created successfully");
         Swal.fire({
           position: "top-end",
           icon: "success",
           title: "Account created successfully!",
           showConfirmButton: false,
-          timer: 150000,
+          timer: 3000,
         });
-       window.location.href = "./dashboard/index.html"
+        window.location.href = "./dashboard/index.html"
+        try {
+              const docRef = await addDoc(collection(db, "users"), {
+                ...userData,
+                uId: user.uid,
+              });
+              console.log("Document written with ID: ", docRef.id);
+            } catch (e) {
+              console.error("Error adding document: ", e);
+            }
+             // try {
+      //   await setDoc(doc(db,"users",user.uid), {
+      //     ...userData,
+      //     uId: user.uid
+      //   });
+      //       console.log("Document written with ID: ", user.uid);
+      //     } catch (e) {
+      //       console.error("Error adding document: ", e);
+      //     }
       })
       .catch((error) => {
-        alert(error.code)
+        console.log(error.message);
+        alert(error.code);
       });
-  };
-  if(window.location.pathname=="/index.html"){
+  } else {
+    // alert("Invalid email or Password");
+    Swal.fire({
+      title: "Invalid",
+      text: "Invalid Email or Password",
+      icon: "question",
+      timer: 3000,
+    });
+  }
+  if (password !== cPassword) {
+    // alert("Passwords should be identical");
+    Swal.fire({
+      title: "Not Match",
+      text: "IPasswords should be identical",
+      icon: "question",
+      timer: 3000,
+    });
+  }
+};
+// if(window.location.pathname=="/index.html"){
+//     let signUp_btn = document.getElementById("signUp_btn");
+//     signUp_btn.addEventListener("click", signUp);
+// }
+// let signUp_btn = document.getElementById("signUp_btn");
+//     signUp_btn.addEventListener("click", signUp);
+if (window.location.pathname == "/index.html") {
+  document.addEventListener('DOMContentLoaded', function () {
+    const signUp_btn = document.getElementById('signUp_btn'); // Replace 'signUp_btn' with your actual element ID
+    if (signUp_btn) {
+      signUp_btn.addEventListener("click", signUp);
+    } else {
+      console.error("Element with ID 'signUp_btn' not found.");
+    }
+  });
+}
+
+let logIn = () => {
+  let email = document.getElementById("Email").value;
+  let password = document.getElementById("pass").value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then(async(userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+
+      // alert("Login Successful")
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Account created successfully!",
+        timer: 3000,  
+
+      });
+      window.location.href = "./dashboard/index.html"
+      // try {
+      //   await setDoc(doc(db,"users",user.uid), {
+      //     ...userData,
+      //     uId: user.uid
+      //   });
+      //       console.log("Document written with ID: ", user.uid);
+      //     } catch (e) {
+      //       console.error("Error adding document: ", e);
+      //     }
+      })
+   
+    .catch((error) => {
+      alert(error.code)
+    });
+};
+if (window.location.pathname == "/index.html") {
   let login_btn = document.getElementById("login_btn");
   login_btn.addEventListener("click", logIn);
-  }
-  let googleSignup=()=>{
-      signInWithPopup(auth, provider)
-      .then((result) => {
-       
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        const user = result.user;
-       console.log(user);
+}
+// let login_btn = document.getElementById("login_btn");
+// login_btn.addEventListener("click", logIn);
+let googleSignup = () => {
+  signInWithPopup(auth, provider)
+    .then(async(result) => {
+
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log(user);
        Swal.fire({
         position: "top-end",
         icon: "success",
         title: "Account created successfully!",
-        showConfirmButton: false,
-        timer: 150000,
+        timer: 3000,
       });
-       window.location.href = "./dashboard/index.html"
+      window.location.href = "./dashboard/index.html"
+      try {
+        await setDoc(doc(db, "users", user.uid), {
+          uid:user.uid,
+          name : user.displayName,
+          email : user.email,
+          image : user.photoURL,
+          number : user.phoneNumber
   
-      }).catch((error) => {
-        
-        const email = error.customData.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        console.log(email, credential);
-      });
-  }
-
-  
-  if(window.location.pathname=="/index.html"){
-      let googleBtn = document.getElementById("googleBtn");
-      googleBtn.addEventListener("click", googleSignup);
-  }
-  console.log(window.location);
-  if(window.location.pathname=="/dashboard/index.html"){
-    document.getElementById("logOut").addEventListener("click",()=>{
-      signOut(auth).then(() => {
-          // alert("Log out successfull")
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "log out successfully!",
-            
-          });
-          window.location.href = "../index.html"
-        }).catch((error) => {
-          console.log(error.code);
-          
         });
-  })
+          console.log("Document written with ID: ", user.uid);
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
 
+    }).catch((error) => {
+
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log(email, credential);
+    });
+}
+
+
+if (window.location.pathname == "/index.html") {
+  let googleBtn = document.getElementById("googleBtn");
+  googleBtn.addEventListener("click", googleSignup);
+}
+// let googleBtn = document.getElementById("googleBtn");
+// googleBtn.addEventListener("click", googleSignup);
+// console.log(window.location);
+// if(window.location.pathname=="/dashboard/index.html"){
+//   document.getElementById("logOut").addEventListener("click",()=>{
+//     signOut(auth).then(() => {
+//         alert("Log out successfull")
+//         // Swal.fire({
+//         //   position: "top-end",
+//         //   icon: "success",
+//         //   title: "log out successfully!",
+//         //   timer: 3000,
+
+//         // });
+//         window.location.href = "../index.html"
+//       }).catch((error) => {
+//         console.log(error.code);
+
+//       });
+// })
+
+// }
+//   document.getElementById("logOut").addEventListener("click",()=>{
+//     signOut(auth).then(() => {
+//         alert("Log out successfull")
+//         // Swal.fire({
+//         //   position: "top-end",
+//         //   icon: "success",
+//         //   title: "log out successfully!",
+//         //   timer: 3000,
+
+//         // });
+//         window.location.href = "../index.html"
+//       }).catch((error) => {
+//         console.log(error.code);
+
+//       });
+// })
+document.addEventListener('DOMContentLoaded', function () {
+  // Your JavaScript code that interacts with the DOM goes here
+  const myElement = document.getElementById('logOut');
+  if (myElement) {
+    myElement.addEventListener('click', function () {
+      // Handle the click event
+      signOut(auth).then(() => {
+        alert("Log out successfull")
+        // Swal.fire({
+        //   position: "top-end",
+        //   icon: "success",
+        //   title: "log out successfully!",
+        //   timer: 3000,
+
+        // });
+        window.location.href = "../index.html"
+      }).catch((error) => {
+        console.log(error.code);
+
+      });
+    });
   }
+});
 // Check current path for debugging
 // console.log("Current path:", window.location.pathname);
 
@@ -196,3 +298,10 @@ if(window.location.pathname=="/index.html"){
 //     console.warn("Script not running because path is not index.html or root.");
 // }
 // console.log(window.location);
+let getAllUsers = async () => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => `,doc.data());
+  });
+};
+getAllUsers()
--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";;
 import { getAuth, createUserWithEmailAndPassword,  signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore, collection, addDoc,getDocs ,doc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
+
 
 
 const firebaseConfig = {
@@ -14,4 +16,6 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  export { getAuth, auth, createUserWithEmailAndPassword,  signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, provider, signOut}
+
+  const db = getFirestore(app);
+  export { getAuth, auth, createUserWithEmailAndPassword,  signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, provider, signOut, getFirestore,db ,collection, addDoc,getDocs, doc, setDoc}
